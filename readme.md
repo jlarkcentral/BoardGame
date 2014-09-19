23cd06172fa2b80645799ce4f3a9f6221c4853bf
jlarkcentral <j.lark.central@gmail.com>
2014-09-20 00:36:45 +0200

    working rhythm panel prototype

---
+++ readme.md
@@ -0,0 +1,10 @@
+Board game / Rhythm game / RPG in Lua Love2d
+
+* TODO
+
+- clean rhythm branch
+- make power ups active
+
+
+currently used soundtrack : "Yellow meadow" by Win920
+https://www.jamendo.com/en/track/1146234/yellow-meadow 
---
+++ rhythmPanel.lua
@@ -0,0 +1,58 @@
+RhythmPanel = {}
+
+
+-- Constructor
+function RhythmPanel:new()
+
+    local object = {
+    
+    imageBackground = love.graphics.newImage('img/rhythmPanel/background.png'),
+    imageFrameIdle = love.graphics.newImage('img/rhythmPanel/idle.png'),
+    imageFrameOk = love.graphics.newImage('img/rhythmPanel/ok.png'),
+    imageFrameWrong = love.graphics.newImage('img/rhythmPanel/wrong.png'),
+
+    icons = {
+	    ['up']     = love.graphics.newImage('img/rhythmPanel/up.png'),
+	    ['down']   = love.graphics.newImage('img/rhythmPanel/down.png'),
+	    ['left']   = love.graphics.newImage('img/rhythmPanel/left.png'),
+	    ['right']  = love.graphics.newImage('img/rhythmPanel/right.png'),
+    	['arrow']  = love.graphics.newImage('img/rhythmPanel/arrow.png'),
+    	['shield'] = love.graphics.newImage('img/rhythmPanel/shield.png')
+	    },
+
+    moves = {},
+    currentMove = '',
+    speed = 2.5,
+    next_in = 1
+
+    }
+    setmetatable(object, { __index = RhythmPanel })
+    return object
+end
+
+function RhythmPanel:update(player)
+	if #self.moves == 0 or self.moves[#self.moves][2] > self.next_in*100 then
+		local move = choose(tables_concat(player:possible_moves(), {'arrow','shield'}))
+		table.insert(self.moves, {move, 0})
+		self.next_in = choose({1,2,3})
+	end
+	if #self.moves > 0 then
+		if self.moves[1][2] > 550 then
+			table.remove(self.moves, 1)
+		end
+		self.currentMove = self.moves[1]
+	end
+	-- (player) update probas ...
+end
+
+function RhythmPanel:draw()
+	love.graphics.draw(self.imageBackground, 800, 0)
+	-- if ... then
+	-- else
+	love.graphics.draw(self.imageFrameIdle, 800, 500)
+	-- end
+	for _,m in ipairs(self.moves) do
+		love.graphics.draw(self.icons[m[1]], 800, m[2])
+		m[2] = m[2] + self.speed
+	end
+end
