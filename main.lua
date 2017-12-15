4044e7be501683891c6b33d5c1830c17f0d90427
jolark <joseph.lark.jl@gmail.com>
2017-12-15 22:46:47 +0100

    escape ?

--- main.lua
+++ main.lua
@@ -6,7 +6,6 @@ function love.load()
 
 	-- load all modules
 	require "player"
-	require "computer"
 	require "board"
 	require "utils"
 
@@ -17,12 +16,6 @@ function love.load()
 
     player = Player:new()
     player:add_ranger()
-    player:add_ranger()
-    player.turn = true
-    player.turnPoints = 5
-
-    computer = Computer:new()
-    computer:add_rogue()
 
     board = Board:new()
 
@@ -33,31 +26,19 @@ function restart()
     board:initialize()
     --board:add_item()
     player:go_to_start_position(board)
-    computer:go_to_start_position(board)
 end
 
 function love.update(dt)
 	player:update(board)
-    computer:update(board, player)
 	board:update(player)
     if love.keyboard.isDown("escape") then
         restart()
-    end
-    if player.turn and player.turnPoints == 0 then
-        computer.turnPoints = 5
-        computer.turn = true
-        player.turn = false
-    elseif computer.turn and computer.turnPoints == 0 then
-        player.turnPoints = 5
-        computer.turn = false
-        player.turn = true
     end
 end
 
 function love.draw()
 	board:draw(player)
 	player:draw()
-    computer:draw()
 	-- board:drawDecoration(player)
 
 	love.graphics.draw(player:current_char().image, 0, 650, 0, 2)
@@ -70,7 +51,6 @@ function love.draw()
     love.graphics.print(tostring("player currentChar: "..player.currentChar), 200, 680)
     love.graphics.print(tostring("selected tile: "..board.selectedTile[1] .. "," .. board.selectedTile[2]), 200, 690)
     love.graphics.print(tostring("selected tile type: "..board:get_tile(board.selectedTile[1], board.selectedTile[2]).tileType), 200, 700)
-    love.graphics.print(tostring("player turn points: "..player.turnPoints), 200, 710)
-    love.graphics.print(tostring("computer turn points: "..computer.turnPoints), 400, 710)
+
 
 end
